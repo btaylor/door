226374758af9c0156015ab59732091c8cb2102c0
Brad Taylor <brad@getcoded.net>
2013-01-05 16:47:34 -0800

    Add handler for when transcription succeeds

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'twilio-ruby'
  
 post '/' do
   response =<<EOF
@@ -7,7 +8,7 @@ post '/' do
 <Response>
   <Play>https://dl.dropbox.com/s/0wf9wok59b5koe2/hello.wav?dl=1</Play>
   <Record timeout="2" maxLength="5" playBeep="false"
-          action="/recordSuccess" />
+          action="/recordSuccess" transcribeCallback="/transcribeSuccess" />
   <Redirect>/recordTimeout</Redirect>
 </Response>
 EOF
@@ -19,7 +20,7 @@ response =<<EOF
 <Response>
   <Play>https://dl.dropbox.com/s/28uq7mlvuf4rw9f/come-on-in.wav?dl=1</Play>
   <Play>http://jetcityorange.com/dtmf/DTMF-9.mp3</Play>
-  <Sms from="+14156305155" to="+19098154939">Hey, I just let someone in the front door.</Sms>
+  <!-- <Sms from="+14156305155" to="+19098154939">Hey, I just let someone in the front door.</Sms> -->
   <Hangup />
 </Response>
 EOF
@@ -35,4 +36,19 @@ response =<<EOF
   <Hangup />
 </Response>
 EOF
+end
+
+post '/transcribeSuccess' do
+  recordingUrl = params[:RecordingUrl]
+  transcriptionText = params[:TranscriptionText]
+
+  accountSid = ENV['TWILIO_ACCOUNT_SID']
+  authToken = ENV['TWILIO_AUTH_TOKEN']
+
+  client = Twilio::REST::Client.new accountSid, authToken
+  client.account.sms.messages.create(
+    :from => ENV['TWILIO_SMS_FROM'],
+    :to => ENV['TWILIO_SMS_TO'],
+    :body => "This is Door. Just let someone in: #{transcriptionText}. #{recordingUrl}",
+  )
 end
